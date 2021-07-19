fa11045454b4dd011e607148422c3a055afcb998
NikStor03 <67559040+NikStor03@users.noreply.github.com>
2021-07-19 14:29:08 +0300

    Update string_filter.py

--- string_filter.py
+++ string_filter.py
@@ -1,4 +1,5 @@
 import datetime
+from typing import Union
 
 
 class Censore:
@@ -15,47 +16,27 @@ class Censore:
             list_of_words = f.read()
             return list_of_words.split("\n")
 
-    def _alone_words(self, string: str) -> None:
+    def _filter(self, string: str) -> None:
         """
-            if word just wrote like "alone". It will detecte it!
-            Before: "Fuck man"
-            After: "**** man"
+            if word just wrote like "alone" or near other words. It will detecte it!
+            Before: "Fuck manshit"
+            After: "**** man****"
         """
-        string_strip = string.split()
-
-        for item in string_strip:
-            if item in self.bad_words:
-                self.find_bad_words.append(item)
-
-    def _deep_filter(self, string: str) -> None:
-        """
-            if word masking near another words, this function will detecte it!
-            Before: "Fuckyou"
-            After:  "****you"
-        """
-        # Deep filter
         for word in self.bad_words:
-            i = 0
-            for _ in string:
-                if not len(word) > len(string):
-                    if string[i:len(word) + i] in self.bad_words:
-                        self.find_bad_words.append(
-                            string[i:len(word) + i])
-                    i += 1
+            if word in string:
+                self.find_bad_words.append(word)
 
     def censore(
         self,
         string: str,
-        custom_words: list = None,
+        custom_words: Union[list, None] = None,
         masking_symbol: str = "*"
     ) -> str:
         if custom_words is not None:
             for word in custom_words:
                 self.bad_words.append(word)
 
-        self._alone_words(string.lower())
-        self._deep_filter(string.lower())
-        # Replace word to masking_symbol
+        self._filter(string.lower())
         for word in self.find_bad_words:
             string = string.replace(word, masking_symbol * len(word))
 
@@ -71,8 +52,8 @@ if __name__ == "__main__":
     cen = Censore()
     print("Load file --", datetime.datetime.now() - start)
 
-    cen = cen.censore("A peace of shit")
+    start = datetime.datetime.now()
+    censor = cen.censore("A peace of shit")
     print("Detecte words --", datetime.datetime.now() - start)
     # A peace of ****
-
-    print(cen)
+    print(censor)
